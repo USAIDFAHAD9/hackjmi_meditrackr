b4f1f4d36b0dd0371a59ec726d0cf2b0e9361d56
Usaid Fahad <usaidfahad9@gmail.com>
2024-02-24 09:54:40 +0530

    added react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-between h-screen">
+      <Navbar />
+      <div className="text-center">
+        <div>helo</div>
+        <div>helo</div>
+        <div>helo</div>
+        <div>helo</div>
+        <div>helo</div>
+      </div>
+      <Footer />
     </div>
-  );
+  )
 }
-
-export default App;
